ff9c1f78415fb444368a8c7ea228a9f3e73f7d45
Janelle K Hirano <jaahnelle@gmail.com>
2019-09-07 11:06:07 -1000

    finished

--- index.js
+++ index.js
@@ -5,20 +5,6 @@ function locate(array, target) {
   // arr3 = ['a', ['b'], 'c', ['d'], ['e']]; // locate e - true
   // arr4 = ['a', 'b', ['c', ['d'], ['e']]]; // locate e - true
 
-  // for loop or while loop? or nested for loop?
-  // if any array includes [i], return true, else return false
-
-  // for (let i = 0; length = array.length; i < length; i++) {
-  //   for (let j = 0; length2 = array[i].length; j < length2; j++) {
-  //     if (array.includes([i])) {
-  //       return true;
-  //     } else {
-  //       return false;
-  //     }
-  //   }
-  //   // console.log(array[i][j]);
-  // }
-
   // returns boolean
   for (let i = 0; i < array.length; i++) {
     if (array[i] === target) {
